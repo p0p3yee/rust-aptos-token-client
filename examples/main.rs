977cbb116d709b1cd5499c8ff1f064c1bbee5112
p0p3yee <devp0p3yee@gmail.com>
2022-09-18 23:14:13 -0400

    Add example

--- examples/main.rs
+++ examples/main.rs
@@ -1,8 +1,8 @@
 use std::str::FromStr;
 
-use aptos_sdk::{rest_client::{Client, FaucetClient}, types::LocalAccount, coin_client::CoinClient};
+use aptos_sdk::{rest_client::{Client, FaucetClient, aptos_api_types::U64}, types::LocalAccount, coin_client::CoinClient};
 use once_cell::sync::Lazy;
-use rust_aptos_token_client::*;
+use rust_aptos_token_client::{types::{TokenId, TokenDataId}, TokenClient};
 use anyhow::{Context, Result};
 use url::Url;
 
@@ -17,14 +17,17 @@ async fn main() -> Result<()> {
     let token_client = TokenClient::new(&rest_client).await.context("Failed to create token client")?;
 
     let mut alice = LocalAccount::generate(&mut rand::rngs::OsRng);
+    let mut bob = LocalAccount::generate(&mut rand::rngs::OsRng);
 
     let collection_name = "Example Collection";
     let token_name = "First NFT !";
 
     println!("\n=== Address ===");
     println!("Alice: {}", alice.address().to_hex_literal());
+    println!("Bob: {}", bob.address().to_hex_literal());
 
     faucet_client.fund(alice.address(), 20_000).await.context("Failed to fund Alice's account")?;
+    faucet_client.fund(bob.address(), 20_000).await.context("Failed to fund Bob's account")?;
 
     println!("\n=== Initial Balances ===");
     println!(
@@ -33,6 +36,13 @@ async fn main() -> Result<()> {
             .get_account_balance(&alice.address())
             .await
             .context("Failed to get Alice's account balance")?
+    );
+    println!(
+        "Bob: {:?}",
+        coin_client
+            .get_account_balance(&bob.address())
+            .await
+            .context("Failed to get Bob's account balance")?
     );
 
     println!("\n=== Creating NFT Collection ===");
@@ -89,9 +99,73 @@ async fn main() -> Result<()> {
         token_name.to_string(),
         None,
     )
-    .await.context("Token not found ?")?;
+    .await?;
     
     println!("NFT Token Data: {:?}", data);
+
+    println!("\n=== Offering Token from Alice to Bob ===");
+
+    let creator_address = alice.address();
+
+    let tx_hash = token_client.offer_token(
+        &mut alice,
+        bob.address(),
+        creator_address,
+        collection_name.to_string(),
+        token_name.to_string(),
+        1,
+        None,
+        None,
+    )
+    .await?;
+
+    rest_client.wait_for_transaction(&tx_hash).await.context("Failed on waiting offer token tx")?;
+
+    println!("\n=== Accepting Token ===");
+
+    let tx_hash = token_client.claim_token(
+        &mut bob,
+        alice.address(),
+        creator_address,
+        collection_name.to_string(),
+        token_name.to_string(),
+        None,
+        None
+    ).await?;
+
+    rest_client.wait_for_transaction(&tx_hash).await.context("Failed on waiting claim token tx")?;
+
+    println!("\n=== Bob Accepted token offered by Alice ===");
+
+    let data = token_client.get_token_for_account(
+        bob.address(),
+        TokenId {
+            token_data_id: TokenDataId {
+                creator: creator_address,
+                collection: collection_name.to_string(),
+                name: token_name.to_string(),
+            },
+            property_version: U64(0u64),
+        }
+    ).await?;
+
+    println!("Bob's Token Data: {:?}", data);
+
+    let result = token_client.get_token_for_account(alice.address(), TokenId {
+        token_data_id: TokenDataId {
+            creator: creator_address,
+            collection: collection_name.to_string(),
+            name: token_name.to_string(),
+        },
+        property_version: U64(0u64),
+    })
+    .await;
+
+    if result.is_err() {
+        println!("Expected Token Not Found Error | Token already transferred to Bob.")
+    } else {
+        println!("Unexpected, Token should be transferred to Bob");
+    }
 
     Ok(())
 }
